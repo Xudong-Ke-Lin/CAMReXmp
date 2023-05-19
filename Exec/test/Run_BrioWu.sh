294fe0aa09999f9196ba2316ef623cee85bc7c4b
Xudong Ke Lin <xk215@melete.lsc.phy.private.cam.ac.uk>
2023-05-19 21:54:49 +0100

    Changed input and running files. Also added the exact Riemann solver in Make.package

--- Exec/test/Run_BrioWu.sh
+++ Exec/test/Run_BrioWu.sh
@@ -10,21 +10,23 @@ for i in "${!array[@]}"
 do
     echo "%s is in %s\n" "${array[i]}" "${array2[i]}"
 
-    mpiexec -n $cpu /data/tycho/xk215/MPHIL_thesis/make_models/$model/./main1d.gnu.MPI.ex inputs l_r=$l_r amr.n_cell=${array[i]}" 10 128" adv.cfl=${array2[i]}  amr.plot_file="/data/tycho/xk215/MPHIL_thesis/"$model"/plt_BrioWu_lr"$l_r"_n_cell"${array[i]}"_cfl"${array2[i]}"_"
+    #mpiexec -n $cpu /data/tycho/xk215/MPHIL_thesis/make_models/$model/./main1d.gnu.MPI.ex inputs l_r=$l_r amr.n_cell=${array[i]}" 10 128" adv.cfl=${array2[i]}  amr.plot_file="/data/tycho/xk215/MPHIL_thesis/"$model"/plt_BrioWu_lr"$l_r"_n_cell"${array[i]}"_cfl"${array2[i]}"_"
 done
-for f in "/data/tycho/xk215/MPHIL_thesis/"$model"/plt_BrioWu_lr"$l_r"_"*; do ~/AMReX/amrex/Tools/Plotfile/./fextract.gnu.ex $f; done
+#for f in "/data/tycho/xk215/MPHIL_thesis/"$model"/plt_BrioWu_lr"$l_r"_"*; do ~/AMReX/amrex/Tools/Plotfile/./fextract.gnu.ex $f; done
 
 l_r=1.0
-array=( 8192 4096 2048 1024 512 )
-array2=( 0.9 0.5 0.5 0.5 0.2 )
+#array=( 8192 4096 2048 1024 512 )
+#array2=( 0.9 0.5 0.5 0.5 0.2 )
+array=( 512 )
+array2=( 0.1 )
 
 for i in "${!array[@]}"
 do
     echo "%s is in %s\n" "${array[i]}" "${array2[i]}"
 
-    mpiexec -n $cpu /data/tycho/xk215/MPHIL_thesis/make_models/$model/./main1d.gnu.MPI.ex inputs l_r=$l_r amr.n_cell=${array[i]}" 10 128" adv.cfl=${array2[i]}  amr.plot_file="/data/tycho/xk215/MPHIL_thesis/"$model"/plt_BrioWu_lr"$l_r"_n_cell"${array[i]}"_cfl"${array2[i]}"_"
+    mpiexec -n $cpu /data/tycho/xk215/MPHIL_thesis/make_models/$model/./CAMREXmp1d.gnu.MPI.ex inputs l_r=$l_r amr.n_cell=${array[i]}" 10 128" adv.cfl=${array2[i]}  amr.plot_file="/data/atalanta/xk215/MPHIL_thesis/"$model"/plt_BrioWu_WENOMUSCLfull_lr"$l_r"_n_cell"${array[i]}"_cfl"${array2[i]}"_"
 done
-for f in "/data/tycho/xk215/MPHIL_thesis/"$model"/plt_BrioWu_lr"$l_r"_"*; do ~/AMReX/amrex/Tools/Plotfile/./fextract.gnu.ex $f; done
+for f in "/data/atalanta/xk215/MPHIL_thesis/"$model"/plt_BrioWu_WENOMUSCLfull_lr"$l_r"_n_cell"*"/"; do ~/AMReX/amrex/Tools/Plotfile/./fextract.gnu.ex $f; done
 
 l_r=0.1
 array=( 8192 4096 2048 1024 512 )
@@ -34,6 +36,6 @@ for i in "${!array[@]}"
 do
     echo "%s is in %s\n" "${array[i]}" "${array2[i]}"
     
-    mpiexec -n $cpu /data/tycho/xk215/MPHIL_thesis/make_models/$model/./main1d.gnu.MPI.ex inputs l_r=$l_r amr.n_cell=${array[i]}" 10 128" adv.cfl=${array2[i]}  amr.plot_file="/data/tycho/xk215/MPHIL_thesis/"$model"/plt_BrioWu_lr"$l_r"_n_cell"${array[i]}"_cfl"${array2[i]}"_"
+    #mpiexec -n $cpu /data/tycho/xk215/MPHIL_thesis/make_models/$model/./main1d.gnu.MPI.ex inputs l_r=$l_r amr.n_cell=${array[i]}" 10 128" adv.cfl=${array2[i]}  amr.plot_file="/data/tycho/xk215/MPHIL_thesis/"$model"/plt_BrioWu_lr"$l_r"_n_cell"${array[i]}"_cfl"${array2[i]}"_"
 done
-for f in "/data/tycho/xk215/MPHIL_thesis/"$model"/plt_BrioWu_lr"$l_r"_"*; do ~/AMReX/amrex/Tools/Plotfile/./fextract.gnu.ex $f; done
+#for f in "/data/tycho/xk215/MPHIL_thesis/"$model"/plt_BrioWu_lr"$l_r"_"*; do ~/AMReX/amrex/Tools/Plotfile/./fextract.gnu.ex $f; done
--- Exec/test/Run_Harris.sh
+++ Exec/test/Run_Harris.sh
@@ -1,8 +1,8 @@
 model=$(cd ../../ && echo ${PWD##*/})
 
-cfl=0.2
-plot_int=2000
-plot_file="/data/melete/xk215/MPHIL_thesis/"$model"/plt_Harris_hyp_anex_fully2_cfl"$cfl
-mpiexec -n 12 /data/tycho/xk215/MPHIL_thesis/make_models/$model/./CAMREXmp2d.gnu.MPI.ex inputs_Harris adv.cfl=$cfl amr.plot_int=$plot_int amr.plot_file=$plot_file"_" 
-
+cfl=0.1
+plot_int=10000
+#plot_int=0
+plot_file="/data/melete/xk215/MPHIL_thesis/"$model"/plt_Harris_WENO_divFree_RK3_Strang2_correctBC_manualBC_veryLowRes_cfl"$cfl
+mpiexec -n 16 /data/tycho/xk215/MPHIL_thesis/make_models/$model/./CAMREXmp2d.gnu.MPI.ex inputs_Harris adv.cfl=$cfl amr.plot_int=$plot_int amr.plot_file=$plot_file"_" amr.n_cell="128 64 0"  #stop_time=25.6037796
 ls -1v $plot_file*/Header | tee $plot_file".visit"
